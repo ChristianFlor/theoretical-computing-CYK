c057bb672c2f7b56bb49da2166936dbf129af3f8
Daniel Fernandez <alejo.dafr@gmail.com>
2020-11-08 15:04:28 -0500

    CYK algorithm works as it should, also fixed read method

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,18 +1,21 @@
 /**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * Icesi University (Cali - Colombia)
- * @author Daniel Alejandro Fernandez Robles <daniel.fernandez1@correo.icesi.edu.co>
+ * @author Daniel Alejandro Fernandez Robles <daniel.fernandez3@correo.icesi.edu.co>
  * @author Christian David Flor Astudillo <christian.flor1@correo.icesi.edu.co>
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 package ui;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.ContextFreeGrammar;
+import model.ContextFreeGrammarOperations;
 
 /**
  * This class 
@@ -23,6 +26,38 @@ public class Main extends Application{
 	 * This class represents
 	 */
 	public static void main(String[] args) {
+		/*Scanner s = new Scanner(System.in);
+		ContextFreeGrammarOperations cfgo = new ContextFreeGrammarOperations();
+		System.out.println("How many variables?");
+		int vars = Integer.parseInt(s.nextLine());
+		System.out.println("Write the productions in this format: V : AB | XY | a | b | ... | ");
+		String input = "";
+		while(vars > 0) {
+			input += s.nextLine() + "\n";
+			vars--;
+		}
+
+		try {
+			ContextFreeGrammar cfg = cfgo.read(input);
+
+			System.out.println("****************");
+			cfg.getProductionRules().forEach((head, bodies) -> {
+				String line = head + " --> ";
+				for(String body : bodies) {
+					line += body + " ";
+				}
+				System.out.println(line);
+			});
+			System.out.println("****************");
+
+			System.out.println("What string will you validate?");
+			System.out.println("RESULT: " + cfgo.CYK(cfg, s.nextLine()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		s.close();
+		*/
+
 		launch();
 	}
 	
