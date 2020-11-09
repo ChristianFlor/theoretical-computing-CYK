55303452b8b2aab23610e4b73bf1f0f6a9a2d1c1
Rolfman <christian.flor1@correo.icesi.edu.co>
2020-11-08 20:13:08 -0500

    cyk algorithm implemented with interface working correctly

--- src/model/ContextFreeGrammarOperations.java
+++ src/model/ContextFreeGrammarOperations.java
@@ -9,6 +9,8 @@ public class ContextFreeGrammarOperations {
     // TODO All in english
     // TODO Exception is too general
     // FIXME Take into account the above comments
+    private HashSet<Character>[][] cykMatrix;
+
     public ContextFreeGrammar read(String source) throws Exception {
         ContextFreeGrammar cfg = new ContextFreeGrammar();
         String[] lines = source.split("\n");
@@ -52,7 +54,7 @@ public class ContextFreeGrammarOperations {
             return cfg.getProductionRules().get(ContextFreeGrammar.START).contains(w);
         }
         int n = w.length();
-        HashSet<Character>[][] cykMatrix = new HashSet[n][n];
+        cykMatrix = new HashSet[n][n];
         // Initialize
         for (int i = 0; i < n; i++) {
             cykMatrix[i][0] = new HashSet<>();
@@ -70,6 +72,7 @@ public class ContextFreeGrammarOperations {
                         checkBodyInProduction(cfg, cykMatrix, body, i, j);
                     }
                 }
+
             }
         }
         return cykMatrix[0][n-1].contains(ContextFreeGrammar.START);
@@ -251,5 +254,9 @@ public class ContextFreeGrammarOperations {
             }
         });
         return newCfg;
+    }
+
+    public HashSet<Character>[][] getCykMatrix() {
+        return cykMatrix;
     }
 }
